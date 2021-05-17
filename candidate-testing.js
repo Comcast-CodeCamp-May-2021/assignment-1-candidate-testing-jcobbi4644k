4cdfe1a6fde41b3e88aa2e95b962e689315ec1a1
jcobbi4644k <>
2021-05-17 04:59:00 +0000

    Assignment 1.5

--- candidate-testing.js
+++ candidate-testing.js
@@ -67,7 +67,6 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
-
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
